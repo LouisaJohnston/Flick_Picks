659c58a583eae1be8729caa84f10eb54c8318af4
LouisaJohnston <louisa.latham.johnston@gmail.com>
2021-03-02 10:30:13 -0800

    setup

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
 /* Required Modules and Variables */
+const express = require("express")
+const app = express();
+const rowdy = require("rowdy-logger")
+const cookieParser = require("cookie-parser")
+const db = require("./models")
+const cryptoJS = require("crypto-js");
+
+require("dotenv").config()
 
 /* Middleware and Config */
+const rowdyRes = rowdy.begin(app)
+app.use(express.static("public"))
+app.use(require("morgan")("tiny"))
+app.set("view engine", "ejs")
+app.use(require("express-ejs-layouts"))
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
 
 /* Routes */
 
+
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log("server started!")
+    rowdyRes.print()
+})
